9c0e7e4adf8e525db3dc8838c3e233c39013294d
Sohag Ahmed <sohagahmed056@gmail.com>
2025-09-22 11:33:10 +0600

    refresh Token To Access token

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -4,6 +4,7 @@ import httpStatus from "http-status-codes"
 import { catchAsync } from "../../utils/catchAsync"
 import { sendResponse } from "../../utils/sendResponse"
 import { AuthServices } from "./auth.service"
+import AppError from "../../errorHelpers/AppError"
 
 const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const loginInfo = await AuthServices.credentialsLogin(req.body)
@@ -16,6 +17,27 @@ const credentialsLogin = catchAsync(async (req: Request, res: Response, next: Ne
     })
 })
 
+
+
+
+const getAccessToken = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+
+    const refreshToken= req.cookies.refreshToken
+    if(!refreshToken){
+        return next(new AppError(httpStatus.BAD_REQUEST, "Please provide refresh token"))
+    }
+    const TokenInfo = await AuthServices.getAccessToken(refreshToken)
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "User Logged In Successfully",
+        data: TokenInfo,
+    })
+})
+
+
 export const AuthControllers = {
-    credentialsLogin
+    credentialsLogin,
+    getAccessToken
 }
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -2,9 +2,11 @@ import bcryptjs from "bcryptjs";
 import httpStatus from "http-status-codes";
 import { envVars } from "../../config/env";
 import AppError from "../../errorHelpers/AppError";
-import { generateToken } from "../../utils/jwt";
-import { IUser } from "../user/user.interface";
+import { generateToken, verifyToken } from "../../utils/jwt";
+import { IsActive, IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
+import { CreateUserToken } from "../../utils/userToken";
+import { JwtPayload } from "jsonwebtoken";
 
 
 const credentialsLogin = async (payload: Partial<IUser>) => {
@@ -21,12 +23,53 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
     if (!isPasswordMatched) {
         throw new AppError(httpStatus.BAD_REQUEST, "Incorrect Password")
     }
+    // const jwtPayload = {
+    //     userId: isUserExist._id,
+    //     email: isUserExist.email,
+    //     role: isUserExist.role
+    // }
+    // const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES)
+    // const refreshToken = generateToken(jwtPayload, envVars.JWT_REFRESH_SECRET,envVars.JWT_REFRESH_EXPIRES)
+
+    const userTokens = CreateUserToken(isUserExist)
+
+    const {password: pwd, ...userData} = isUserExist.toObject()
+
+    return {
+        accesToken: userTokens.accessToken,
+        refreshToken: userTokens.refreshToken,
+        user: userData
+    }
+
+}
+
+
+
+
+const giveAccessToken = async (refreshToken: string) => {
+    //verify refresh token
+    const verfiedRefreshToken = verifyToken(refreshToken, envVars.JWT_REFRESH_SECRET)
+
+    const isUserExist = await User.findOne({ email: verfiedRefreshToken.email}) 
+    if (!isUserExist) {
+        throw new AppError(httpStatus.BAD_REQUEST, "Email does not exist")
+    }
+
+    if(isUserExist.isActive === IsActive.BLOCKED){
+        throw new AppError(httpStatus.FORBIDDEN, "Your account has been blocked. Please contact support.")
+    }
+
+   
     const jwtPayload = {
         userId: isUserExist._id,
         email: isUserExist.email,
         role: isUserExist.role
     }
     const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES)
+    
+
+
+
 
     return {
         accessToken
@@ -34,8 +77,11 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
 
 }
 
+
 //user - login - token (email, role, _id) - booking / payment / booking / payment cancel - token 
 
 export const AuthServices = {
-    credentialsLogin
+    credentialsLogin,
+
+    getAccessToken: giveAccessToken
 }
